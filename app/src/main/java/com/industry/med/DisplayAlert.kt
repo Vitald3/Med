d08dbfe2766135ccb852397f374309ce47a79a4d
Виталий Хайченко <vitald333@gmail.com>
2023-06-02 16:30:35 +0300

    Change 0.12

--- app/src/main/java/com/industry/med/DisplayAlert.kt
+++ app/src/main/java/com/industry/med/DisplayAlert.kt
@@ -7,9 +7,9 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 
 class DisplayAlert : AppCompatActivity() {
-    internal var title: String? = null
-    internal var body: String? = null
-    internal var guid: String? = null
+    private var title: String? = null
+    private var body: String? = null
+    private var guid: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,15 +19,24 @@ class DisplayAlert : AppCompatActivity() {
             body = b!!.getString("body")
             guid = b!!.getString("guid")
             val builder: AlertDialog.Builder = AlertDialog.Builder(this)
-            builder.setTitle(title)
-            builder.setMessage(body).setCancelable(false).setPositiveButton("Открыть") { _, _ ->
-                val addIntent = Intent(this, CallActivity::class.java)
-                addIntent.putExtra("guid", guid)
-                addIntent.putExtra("cord", false)
-                ContextCompat.startActivity(this, addIntent, null)
-                finish()
-            }.setNegativeButton("Закрыть") {_, _ ->}
+
+            builder.setTitle(title).setMessage(body)
+                .setCancelable(false)
+                .setNegativeButton("Закрыть") { _, _ ->
+                    finish()
+                }
+                .setPositiveButton("Открыть") { _, _ ->
+                    val addIntent = Intent(this, CallActivity::class.java)
+                    addIntent.putExtra("guid", guid)
+                    addIntent.putExtra("cord", false)
+                    ContextCompat.startActivity(this, addIntent, null)
+                    finish()
+                }
+
             val alertDialog = builder.create()
+            alertDialog.setOnDismissListener {
+                finish()
+            }
             alertDialog.show()
         }
     }
